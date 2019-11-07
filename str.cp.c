37872941216890c8d6e2728ae7a0f5eee64d85d7
aleksandrs855 <54942906+aleksandrs855@users.noreply.github.com>
2019-11-07 15:40:17 +0200

    df
    dgf

---
+++ str.cp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char s1[50]
+	char s2[50]
+
+	strcpy(s1, "teksts");
+	strcpy(s2, s1);
+
+	printf("%s\n", s2);
+
+	return 0;
+
+}
---
+++ string.cp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+char str1[50];
+char str2[50];
+
+printf(str1, "tavs teksts: ");
+scanf("%s", text);
+printf("tavs teksts %s.", )
+
+strcpy(str1, "%s");
+strcpy(srt2, str1);
+
+return 0;
+}
